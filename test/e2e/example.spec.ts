c3d31110a5c802c21ffc3edecf26fd77613bec17
Yura Chemeris <yurachemeris@Yuras-MacBook-Pro.local>
2024-03-13 11:04:56 +0200

    add playwright workflow 2

--- test/e2e/example.spec.ts
+++ test/e2e/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("/");
+  await page.goto("/sdfgs");
 
   // Expect a title "to contain" a substring.
   expect(await page.screenshot()).toMatchSnapshot('sss.png')
