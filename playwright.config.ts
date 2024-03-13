a07f3fedf9ca329643a2b2aec6f8de4b28c75b59
Yura Chemeris <yurachemeris@Yuras-MacBook-Pro.local>
2024-03-13 15:26:13 +0200

    asa 6667dfaef

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,14 +9,14 @@ import { defineConfig, devices } from "@playwright/test";
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-const isCI = !!process.env.CI;
-
 export default defineConfig({
   testDir: "./test/e2e",
-  workers: isCI ? 1 : "50%",
-  retries: isCI ? 2 : 0,
-  forbidOnly: isCI,
-  outputDir: ".test/spec/output",
+  /* Run tests in files in parallel */
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  outputDir: ".test/spec/output/",
   snapshotPathTemplate:
     ".test/spec/snaps/{projectName}/{testFilePath}/{arg}{ext}",
   testMatch: "*.spec.{ts,tsx}",
@@ -29,9 +29,18 @@ export default defineConfig({
         open: "never",
       },
     ],
-    isCI ? ["github"] : ["line"],
+    process.env.CI ? ["github"] : ["line"],
   ],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: "http://127.0.0.1:3000",
+    screenshot: "only-on-failure",
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: "on-first-retry",
+  },
 
+  /* Configure projects for major browsers */
   projects: [
     {
       name: "Desktop Chrome (1920x1080)",
@@ -56,6 +65,7 @@ export default defineConfig({
     },
   ],
 
+  /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
     url: "http://127.0.0.1:3000",
