f6f072379c1afcf7fdc3d74ff5ec05ddda9dc6a5
Yura Chemeris <yurachemeris@Yuras-MacBook-Pro.local>
2024-03-13 12:51:39 +0200

    added 2

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,16 +21,16 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   updateSnapshots: "missing",
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  outputDir: "./test/e2e/__snapshots__/output",
+  outputDir: ".test/spec/output",
   snapshotPathTemplate:
-    "./test/e2e/__snapshots__/snaps/{projectName}/{testFilePath}/{arg}{ext}",
+    ".test/spec/snaps/{projectName}/{testFilePath}/{arg}{ext}",
   testMatch: "*.spec.{ts,tsx}",
 
   reporter: [
     [
       "html",
       {
-        outputFolder: "./test/e2e/__snapshots__/results",
+        outputFolder: ".test/spec/results",
         open: "never",
       },
     ],
