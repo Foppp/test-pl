5925fbc58caefeab83bddabb666b77e28927ce8f
Yura Chemeris <yurachemeris@Yuras-MacBook-Pro.local>
2024-03-19 17:28:02 +0200

    vis

--- test/e2e/example.spec.ts
+++ test/e2e/example.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
+  // testinfo.snapshotSuffix = '';
   await page.goto("/sdfgs");
 
   await expect(page).toHaveTitle(/titleContent/);
