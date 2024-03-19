e70bb33a01a76d64c60b8b3b9e34046a4a48116c
Yura Chemeris <yurachemeris@Yuras-MacBook-Pro.local>
2024-03-19 17:32:19 +0200

    vis2

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,30 +12,20 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./test/e2e",
   /* Run tests in files in parallel */
+  // fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  outputDir: ".test/spec/output/",
-  snapshotPathTemplate:
-    ".test/spec/snaps/{projectName}/{testFilePath}/{arg}{ext}",
-  testMatch: "*.spec.{ts,tsx}",
-
-  reporter: [
-    [
-      "html",
-      {
-        outputFolder: ".test/spec/results",
-        open: "never",
-      },
-    ],
-    process.env.CI ? ["github"] : ["line"],
-  ],
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [["html", { open: "never" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://127.0.0.1:3000",
-    screenshot: "only-on-failure",
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
@@ -43,25 +33,28 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "Desktop Chrome (1920x1080)",
-      use: {
-        ...devices["Desktop Chrome"],
-        viewport: { width: 1920, height: 1080 },
-      },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
+    /* Test against mobile viewports. */
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
     },
     {
-      name: "Desktop Firefox (1920x1080)",
-      use: {
-        ...devices["Desktop Firefox"],
-        viewport: { width: 1920, height: 1080 },
-      },
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
+
+    /* Test against branded browsers. */
+    {
+      name: 'Microsoft Edge',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' },
     },
     {
-      name: "Desktop Safari (1920x1080)",
-      use: {
-        ...devices["Desktop Safari"],
-        viewport: { width: 1920, height: 1080 },
-      },
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     },
   ],
 
--- test/e2e/example.spec.ts
+++ test/e2e/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  // testinfo.snapshotSuffix = '';
+test("has title", async ({ page }, testinfo) => {
+  testinfo.snapshotSuffix = '';
   await page.goto("/sdfgs");
 
   await expect(page).toHaveTitle(/titleContent/);
