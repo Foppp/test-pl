8619f030e11fa44190f22f02a0670c45ddc66ede
Yura Chemeris <yurachemeris@Yuras-MacBook-Pro.local>
2024-03-19 17:35:58 +0200

    vis3

--- test/e2e/example.spec.ts
+++ test/e2e/example.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }, testinfo) => {
-  testinfo.snapshotSuffix = '';
+  testinfo.snapshotSuffix = "";
   await page.goto("/sdfgs");
 
   await expect(page).toHaveTitle(/titleContent/);
-  expect(await page.screenshot()).toMatchSnapshot("content.png");
+  expect(await page.screenshot()).toMatchSnapshot("content.png", {
+    maxDiffPixels: 2,
+  });
 });
