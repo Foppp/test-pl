e09e213bd679ec312099a0c0f55aa00f4afd7dd3
Yura Chemeris <yurachemeris@Yuras-MacBook-Pro.local>
2024-10-07 16:10:32 +0300

    change 6

--- test/e2e/example.spec.ts
+++ test/e2e/example.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from "@playwright/test";
+import { setTimeout } from "timers/promises";
 
 test("has title", async ({ page }) => {
   await page.goto("/");
 
   await expect(page).toHaveTitle(/titleContent/);
-
+  await setTimeout(1000);
   expect(await page.screenshot()).toMatchSnapshot("content.png", {
     maxDiffPixels: 2,
   });
